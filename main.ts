326d6162f11f3c02452228289beee9df6bd383db
mustiercoupes <147052650+mustiercoupes@users.noreply.github.com>
2025-09-18 15:17:27 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    randomNumber = randint(0, 4)
+    if (randomNumber == 4) {
+        basic.showString("DEFINATELY")
+    } else if (randomNumber == 3) {
+        basic.showString("TRY AGAIN")
+    } else if (randomNumber == 2) {
+        basic.showString("YES")
+    } else if (randomNumber == 1) {
+        basic.showString("NO")
+    } else {
+        basic.showString("I DON'T KNOW")
+    }
+    basic.showNumber(8)
+})
+let randomNumber = 0
+basic.showString("ASK A QUESTION")
+basic.showNumber(8)
